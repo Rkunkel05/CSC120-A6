46e0d2d409b65861dd8a9336495fcc0608cada8c
Rachel <rkunkel@smith.edu>
2024-03-05 11:59:12 -0500

    starting 2 work yahoo

--- House.java
+++ House.java
@@ -1,8 +1,29 @@
-/* This is a stub for the House class */
-public class House {
+import java.util.ArrayList;
+
+public class House extends Building {
+
+  ArrayList<String>
 
   public House() {
+    super("Lamont House", "17 Prospect St.", 3);
+    ArrayList<String> residents; // The <String> tells Java what kind of data we plan to store IN the ArrayList
+    private boolean hasDiningRoom;
     System.out.println("You have built a house: 🏠");
+  }
+
+  // checks if there's a dining room in the house 
+  public boolean hasDiningRoom() {
+    if (!diningroom :: House) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+  
+  
+  public int nResidents() {
+    // return num of Residents
+    // return Residents;
   }
 
   public static void main(String[] args) {
