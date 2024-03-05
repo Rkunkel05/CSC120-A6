a9e77d447b1f0a95721fb85b229c415a54726f81
Rachel <rkunkel@smith.edu>
2024-03-05 17:14:22 -0500

    working on library class

--- Library.java
+++ Library.java
@@ -1,12 +1,61 @@
 /* This is a stub for the Library class */
+
+import java.util.Hashtable;
+
 public class Library extends Building {
 
-    public Library() {
+  private Hashtable<String, Boolean> collection;
+  String title;
+
+    public Library(String name, String address, int floors) {
+      super(name, address, floors); 
       System.out.println("You have built a library: 📖");
+      Hashtable<String, boolean>;
     }
-  
+
+    public void addTitle(String title, boolean); {
+      collection.put(key, value);
+    }
+
+    public String removeTitle(String title, boolean); { // return the title that we removed
+      Hash_Table.remove(key, value);
+      return title;
+    }
+
+    public void checkOut(String title);{
+
+    }
+
+    public void returnBook(String title); {
+
+    }
+
+    // returns true if the title appears as a key in the Libary's collection, false otherwise
+    public boolean containsTitle(String title) {
+      if (title :: collection) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    
+    // returns true if the title is currently available, false otherwise
+    public boolean isAvailable(String title) {
+
+    }
+
+    // prints out the entire collection in an easy-to-read way (including checkout status)
+    public void printCollection() {
+      System.out.println(collection);
+    }
+
     public static void main(String[] args) {
-      new Library();
+      Library Neilson = new Library("Neilson Library", "7 Neilson Drive", 5);
+      Neilson.addTitle("Animal Farm by George Orwell", true);
+      Neilson.addTitle("Don Quixote by Miguel de Cervantes", true);
+      Neilson.addTitle("Dune by Frank Herbert", true);
+
+
     }
   
   }
--- reflection.md
+++ reflection.md
@@ -1,4 +1,9 @@
 Use this file to record your reflection on this assignment.
 
 - What are your initial impressions of how `Inheritance` affects your programming?
+I found Inheritance to be very interesting. In a way, I found it made my programming almost a little easier. For instance, I didn't have to write new code each time I wanted to make a new building. Instead, I could just call on the Building parent class to help with that. I'm not entirely sure I completely understand all of Inheritance, but I was at least apply to apply it to an extent!
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+* Reading through the README in its entirety first always = **
+* Jotting down brief notes while getting the project overview in class helped me find my footing
+* Writing down ideas outside of code on paper 
